69b16a79e32f1d1bf8939e74e33c2101630a736a
喻聪 <yucongcong@i-syp.com>
2020-01-16 12:01:31 +0800

    代码提交

---
+++ commodity-api/src/main/java/com/yucong/eureka/controller/CommodityController.java
@@ -0,0 +1,69 @@
+package com.yucong.eureka.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yucong.api.pojo.Commodity;
+import com.yucong.eureka.service.CommodityFeignService;
+
+/**
+ * Application Client中的控制器。是和用户直接交互的控制器。
+ * 像平时开发代码一样。调用本地的一个service接口。通过service接口远程访问Application Service。
+ */
+@RestController
+public class CommodityController {
+
+	/**
+	 * 本地定义的服务接口。用于实现远程调用application  service的接口。
+	 */
+	@Autowired
+	private CommodityFeignService commodityService;
+	
+	/**
+	 * 无参
+	 */
+	@GetMapping("/testFeign")
+	public List<String> testFeign(){
+		System.out.println(commodityService.getClass().getName());
+		return commodityService.testFeign();
+	}
+	
+	/**
+	 * 一个参数，调用远程服务的时候，发起的请求是GET请求
+	 */
+	@GetMapping("/get/{id}")
+	public Commodity getById(@PathVariable("id") Long id){
+		return commodityService.getById(id);
+	}
+	
+	/**
+	 * 一个参数，调用远程服务的时候，发起的请求是POST请求
+	 */
+	@GetMapping("/get")
+	public Commodity getByIdWithPOST(Long id){
+		return commodityService.getByIdWithPOST(id);
+	}
+	
+	/**
+	 * 多个参数，调用远程服务的时候，发起的请求是GET请求
+	 */
+	@GetMapping("/add/{id}/{name}")
+	public Commodity add(@PathVariable("id") Long id, @PathVariable("name") String name){
+		return commodityService.add(id, name);
+	}
+	
+	/**
+	 * 自定义类型参数，调用远程服务的时候，发起的请求是POST请求
+	 * feign技术远程访问application service的时候，默认情况下GET请求不能传递自定义类型参数
+	 */
+	@GetMapping("/add")
+	public Commodity add(Commodity pojo){
+		// return service.add(pojo);
+		return commodityService.addWithPOST(pojo);
+	}
+	
+}
---
+++ commodity-service/src/main/java/com/yucong/eureka/controller/CommodityServiceImpl.java
@@ -0,0 +1,72 @@
+package com.yucong.eureka.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yucong.api.pojo.Commodity;
+import com.yucong.api.service.CommodityService;
+
+/**
+ * 自定义的服务控制器
+ * 对外提供服务的Application Service。
+ * 不能随便的定义服务了。如果想让Application Client可以通过Feign技术访问当前类型提供的服务，
+ * 则必须遵循服务标准。
+ */
+@RestController
+public class CommodityServiceImpl implements CommodityService {
+
+	/**
+	 * 因为当前的方法都是实现接口FirstFeignService中的方法。
+	 * 而在接口中，已经将请求URL和方法耦合到一起了。
+	 * 所以在当前的控制器中，不能重复定义@RequestMapping来约束请求URL。
+	 */
+	@Override
+	public List<String> testFeign() {
+		
+		List<String> result = new ArrayList<>();
+		
+		result.add("test feign");
+		result.add("this is first spring cloud with feign");
+		
+		return result;
+	}
+
+	@Override
+	public Commodity getById(Long id) {
+		return new Commodity(id, "getById");
+	}
+	
+	/**
+	 * 如果方法参数是处理POST请求的JSON数据的。
+	 * 那么还是需要定义@RequestBody注解来描述方法参数的。
+	 */
+	@Override
+	public Commodity getByIdWithPOST(@RequestBody Long id) {
+		return new Commodity(id, "getByIdWithPOST");
+	}
+
+	@Override
+	public Commodity add(Long id, String name) {
+		System.out.println( "add(Long id, String name)" );
+		return new Commodity(id, name);
+	}
+	
+	/**
+	 * 在默认的情况下，Feign不能通过GET请求传递自定义类型的请求参数。
+	 */
+	@Override
+	public Commodity add(@RequestBody Commodity pojo) {
+		System.out.println( "add(@RequestBody Commodity pojo)" );
+		return pojo;
+	}
+
+	@Override
+	public Commodity addWithPOST(@RequestBody Commodity pojo) {
+		System.out.println( "addWithPOST(@RequestBody Commodity pojo)" );
+		return pojo;
+	}
+	
+}
