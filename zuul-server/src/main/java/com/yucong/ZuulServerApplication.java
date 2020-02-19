8f763dc54c656601e1716a9269725f02db3c55cd
yucong <2241268051@qq.com>
2020-02-19 10:02:50 +0800

    zuul网关

---
+++ zuul-server/src/main/java/com/yucong/ZuulServerApplication.java
@@ -0,0 +1,19 @@
+package com.yucong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulServerApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+	
+
+
+}
+
