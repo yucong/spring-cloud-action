b4e580ee3692375639b678d12b45290aee86e787
yucong <2241268051@qq.com>
2020-03-12 13:23:30 +0800

    集成zipkin

---
+++ sleuth-cart/src/main/java/com/yucong/eureka/SleuthCartApplication.java
@@ -0,0 +1,14 @@
+package com.yucong.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class SleuthCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthCartApplication.class, args);
+	}
+	
+}
---
+++ sleuth-search/src/main/java/com/yucong/eureka/SleuthSearchApplication.java
@@ -0,0 +1,14 @@
+package com.yucong.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class SleuthSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthSearchApplication.class, args);
+	}
+	
+}
